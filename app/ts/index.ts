384d1e3b4477a54aa772ca57cedea7ce9e302ca1
daitomiun <mateo.rodriguez.fierro@gmail.com>
2024-11-18 22:21:25 -0500

    Add rotation X and Y to turntable
    HAS TO FIX: new to fix stopping of object when it gets out of the rectable

--- app/ts/index.ts
+++ app/ts/index.ts
@@ -1,3 +1,47 @@
 // TODO here this will be used as an importer to all segmented ts files
 console.log('hello');
 
+let turntableId = document.getElementById("box__rotate");
+
+let lastX: number;
+let lastY: number;
+let currentX = -25;
+let currentY = 25;
+
+let isDragging = false;
+
+turntableId?.addEventListener("mousedown", (e) => {
+	isDragging = true;
+	lastX = e.clientX;
+	lastY = e.clientY;
+
+	e.preventDefault();
+
+});
+
+turntableId?.addEventListener("mouseup", (e) => {
+	isDragging = false;
+});
+
+turntableId?.addEventListener("mouseout", (e) => {
+	isDragging = false;
+});
+
+turntableId?.addEventListener("mousemove", (e) => {
+	if (isDragging) {
+		const deltaX = e.clientX - lastX;
+		const deltaY = e.clientY - lastY;
+
+		currentX += deltaY * 0.5;
+		currentY += deltaX * 0.5;
+
+		turntableId!.style.transform = `rotateX(${currentX}deg) rotateY(${currentY}deg)`;
+
+		lastX = e.clientX;
+		lastY = e.clientY;
+	}
+});
+
+
+
+console.log(turntableId);
--- js_compilation/index.js
+++ js_compilation/index.js
@@ -1,4 +1,34 @@
 "use strict";
 // TODO here this will be used as an importer to all segmented ts files
 console.log('hello');
+let turntableId = document.getElementById("box__rotate");
+let lastX;
+let lastY;
+let currentX = -25;
+let currentY = 25;
+let isDragging = false;
+turntableId === null || turntableId === void 0 ? void 0 : turntableId.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    lastX = e.clientX;
+    lastY = e.clientY;
+    e.preventDefault();
+});
+turntableId === null || turntableId === void 0 ? void 0 : turntableId.addEventListener("mouseup", (e) => {
+    isDragging = false;
+});
+turntableId === null || turntableId === void 0 ? void 0 : turntableId.addEventListener("mouseout", (e) => {
+    isDragging = false;
+});
+turntableId === null || turntableId === void 0 ? void 0 : turntableId.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+        const deltaX = e.clientX - lastX;
+        const deltaY = e.clientY - lastY;
+        currentX += deltaY * 0.5;
+        currentY += deltaX * 0.5;
+        turntableId.style.transform = `rotateX(${currentX}deg) rotateY(${currentY}deg)`;
+        lastX = e.clientX;
+        lastY = e.clientY;
+    }
+});
+console.log(turntableId);
 //# sourceMappingURL=index.js.map
